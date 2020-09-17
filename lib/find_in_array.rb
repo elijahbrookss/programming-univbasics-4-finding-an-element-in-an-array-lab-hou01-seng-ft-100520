02775c0b6531f4daac0894ca8617d30f262acb66
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-17 18:23:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,9 +3,10 @@ require "pry"
 def find_element_index(array, value_to_find)
   index_of_value = nil;
   for i in array do
-    binding.pry
     if (array[i] == value_to_find) then
       index_of_value = i
+      
+      binding.pry
       return index_of_value
     end
   end
