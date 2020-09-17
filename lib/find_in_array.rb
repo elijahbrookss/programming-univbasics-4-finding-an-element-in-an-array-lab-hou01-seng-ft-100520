5110dcc4f505aeca7a5142d3268f0e8006851a9a
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-17 18:28:33 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,12 +3,8 @@
 def find_element_index(array, value_to_find)
   for i in array do
     if (array[i] == value_to_find) then
-      print(i);
       return i
     end
   end
   return nil
 end
-
-
-find_element_index([2, 4, 5, 6], 2);
