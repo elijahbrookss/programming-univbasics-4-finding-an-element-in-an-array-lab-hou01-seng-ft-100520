095319cdd74627789664123a81699faf3a3f61b1
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-17 18:24:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,10 @@
-require "pry"
+
 
 def find_element_index(array, value_to_find)
-  index_of_value = nil;
   for i in array do
     if (array[i] == value_to_find) then
-      index_of_value = i
-            return index_of_value
+      return i
     end
   end
-  return index_of_value
+  return nil
 end
